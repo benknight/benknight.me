c0a0c5890a4968668ecd9ce4720be09d6d89bc90
Benjamin Knight <ben@benknight.me>
2014-11-18 15:17:57 -0800

    Adding height setter thing

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,24 @@
 (function () {
 
+    // We set height to mimick `height: 100vh` in CSS, which is
+    // unfortunately not reliable.
+    // See: https://code.google.com/p/chromium/issues/detail?id=428132
+
+    var mediaQueryList = window.matchMedia('(orientation: portrait)');
+
+    function setMaxHeight (mediaQueryList) {
+      var vh = window.innerHeight;
+      var vw = window.innerWidth;
+      var $siteHeader = $('.site-header');
+
+      if (mediaQueryList.matches) {
+        $siteHeader.css('height', Math.max(vh, vw));
+      } else {
+        $siteHeader.css('height', Math.min(vh, vw));
+      }
+    }
+
+    setMaxHeight(mediaQueryList);
+    mediaQueryList.addListener(setMaxHeight);
+
 })();
