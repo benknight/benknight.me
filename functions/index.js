29ff5c9bb3be37e90d10fd2daf55f1015505c9a9
Benjamin Knight <ben@benknight.me>
2019-09-21 15:04:21 +0700

    Add getLocations function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,28 @@
-const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+// const functions = require('firebase-functions');
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+//   response.send('Hello from Firebase!');
 // });
+const Airtable = require('airtable');
+const functions = require('firebase-functions');
+
+const apiKey = functions.config().airtable.api_key;
+const base = new Airtable({ apiKey }).base('app42Tz6LNdfUEXBP');
+
+exports.getLocations = functions.https.onRequest((request, response) => {
+  base('Location')
+    .select()
+    .firstPage((error, records) => {
+      if (error) {
+        console.error(error);
+        response.status(500).end();
+      }
+      response.json(records.map(r => r.fields)).send();
+    });
+});
+
+exports.postLocation = functions.https.onRequest((request, response) => {
+  // TODO
+});
