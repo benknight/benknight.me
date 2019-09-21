91df4799bfbe3995a2b443bbd64c76d0b0a76142
Benjamin Knight <ben@benknight.me>
2019-09-21 21:53:07 +0700

    Add postLocation function

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,67 @@
 // Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-// const functions = require('firebase-functions');
+// https://firebase.google.com/docs/Functions/write-firebase-Functions
+// const admin = require('firebase-admin');
+// const Functions = require('firebase-functions');
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
+// admin.initializeApp();
+//
+// exports.helloWorld = Functions.https.onRequest((request, response) => {
 //   response.send('Hello from Firebase!');
 // });
+
 const Airtable = require('airtable');
-const functions = require('firebase-functions');
+const Functions = require('firebase-functions');
+const GoogleMaps = require('@google/maps');
 
-const apiKey = functions.config().airtable.api_key;
-const base = new Airtable({ apiKey }).base('app42Tz6LNdfUEXBP');
+const airtableKey = Functions.config().airtable.api_key;
+const googleKey = Functions.config().google.server_key;
+const base = new Airtable({ apiKey: airtableKey }).base('app42Tz6LNdfUEXBP');
+const mapsClient = GoogleMaps.createClient({ key: googleKey, Promise });
 
-exports.getLocations = functions.https.onRequest((request, response) => {
-  base('Location')
-    .select()
-    .firstPage((error, records) => {
-      if (error) {
-        console.error(error);
-        response.status(500).end();
-      }
-      response.json(records.map(r => r.fields)).send();
-    });
+exports.getLocations = Functions.https.onRequest(async (request, response) => {
+  try {
+    const records = await base('Location')
+      .select({ sort: [{ field: 'Date', direction: 'desc' }] })
+      .firstPage();
+    response.json(records.map(r => r.fields)).send();
+  } catch (error) {
+    console.log(error);
+    response.status(500).send();
+  }
 });
 
-exports.postLocation = functions.https.onRequest((request, response) => {
-  // TODO
+exports.postLocation = Functions.https.onRequest(async (request, response) => {
+  const { lat, lng } = request.body;
+  if (!lat || !lng) {
+    response.status(400).end();
+    return;
+  }
+  try {
+    const {
+      json: { results },
+    } = await mapsClient
+      .reverseGeocode({
+        latlng: [lat, lng],
+        result_type: 'locality|administrative_area_level_1',
+      })
+      .asPromise();
+    const locality = results.find(r => r.types.indexOf('locality') !== -1);
+    const adminAreaL1 = results.find(
+      r => r.types.indexOf('administrative_area_level_1') !== -1,
+    );
+    const records = await base('Location').create([
+      {
+        fields: {
+          Date: new Date().toISOString(),
+          Lat: lat,
+          Lng: lng,
+          Address: (locality || adminAreaL1).formatted_address,
+        },
+      },
+    ]);
+    return response.json(records.map(r => r.fields)).send();
+  } catch (error) {
+    console.error(error);
+    return response.status(500).end();
+  }
 });
