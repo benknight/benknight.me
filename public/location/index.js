aba0e9c3f2dcc4f43d46543aac3c23a346391c72
Benjamin Knight <ben@benknight.me>
2019-10-04 21:11:23 +0900

    Location design tweaks

--- public/location/index.js
+++ public/location/index.js
@@ -21,18 +21,12 @@ axios.get('/getLocations').then(response => {
   const mapLg = `https://api.mapbox.com/styles/v1/mapbox/dark-v10/static/pin-l+000(${current.Lng},${current.Lat})/${current.Lng},${current.Lat},5,0,0/640x480@2x?access_token=${mapBoxToken}`;
 
   document.getElementById('root').innerHTML = `
-    <div class="flex flex-column">
-      <a class="order-1-ns mv4-ns" href="${current['Google Maps Link']}">
-        <picture>
-          <source media="(min-width: 425px)" srcset="${mapLg}">
-          <img alt="" class="h-auto" height="480" src="${mapSm}" width="640">
-        </picture>
-      </a>
-      <h1 class="f2 f1-m f-subheadline-l lh-title mv4 mv5-l ph4 ph5-l mw8 center">
-        ${current.Emoji}<br />
-        ${current.Address}
-      </h1>
-    </div>
+    <a class="db mv4-ns" href="${current['Google Maps Link']}">
+      <picture>
+        <source media="(min-width: 425px)" srcset="${mapLg}">
+        <img alt="" class="h-auto" height="480" src="${mapSm}" width="640">
+      </picture>
+    </a>
     <ul class="list ph4 ph5-l mv4 mv5-l tl lh-copy mw8 center">
       ${records
         .map(
