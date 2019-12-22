ab3a458eb40e867b9f779ad39eb8a90f430db6aa
Benjamin Knight <ben@benknight.me>
2019-12-22 11:51:19 +0700

    Delete deployCocolist function

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,8 @@
 const getLocations = require('./getLocations');
 const postLocation = require('./postLocation');
 const sunsetLights = require('./sunsetLights');
-const deployCocolist = require('./deployCocolist');
 
 module.exports = {
-  deployCocolist,
   getLocations,
   postLocation,
   sunsetLights,
