c0e4849671232a4ccb70e7dfc1d53ea5a3241641
Benjamin Knight <ben@benknight.me>
2023-06-19 19:56:19 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,15 @@
+import Script from 'next/script';
 import '../style.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        data-goatcounter="https://benknight.goatcounter.com/count"
+        async
+        src="//gc.zgo.at/count.js"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
